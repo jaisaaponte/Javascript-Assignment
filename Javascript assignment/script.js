a879f1f764a3daea8bdf7e097e870a9bc52f57ca
jaisaaponte <132961636+jaisaaponte@users.noreply.github.com>
2023-07-30 15:48:24 -0400

    Add files via upload

---
+++ Javascript assignment/script.js
@@ -0,0 +1,6 @@
+var firstName = "Jaisa";
+var lastName = "Aponte";
+var age = 21;
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("I am having so much fun in this class!");
